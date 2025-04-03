ae18896ed56b136ed7d05464b10720ae8f139335
MauroPello <mauro.pellonara@gmail.com>
2025-04-03 22:23:32 +0200

    enable things

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt";
+import { Analytics } from '@vercel/analytics/nuxt';
+</script>
+
+<template>
+  <slot />
+  <SpeedInsights/>
+  <Analytics/>
+</template>
+
+<style lang="postcss" scoped>
+</style>
