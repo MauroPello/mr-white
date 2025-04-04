f72dfcf6d0c868b7b4d669507c0d00b6ec721bec
MauroPello <mauro.pellonara@gmail.com>
2025-04-04 19:03:42 +0200

    lets see

--- constants/company.ts
+++ constants/company.ts
@@ -1,7 +1,7 @@
-export const mediaFolder = "/_ipx/_/";
+export const mediaFolder = "/_vercel/image?url=";
 
 export const companyName = "L'Infiltrato / Mr. White";
-export const companyUrl = "http://localhost:3000";
+export const companyUrl = "https://mrwhite.fun";
 export const companySEOTitle = `${companyName} - Gioco di Società Online (Un Telefono)`;
 export const companySEODescription =
   "Gioca online a L'Infiltrato (Mr. White) con amici usando un solo telefono! Scopri chi ha la parola diversa in questo divertente party game di bluff italiano.";
