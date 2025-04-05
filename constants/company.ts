0bf255e0f6aceccbacbe9044b51c62a99e4e80cd
MauroPello <mauro.pellonara@gmail.com>
2025-04-06 01:09:04 +0200

    little better

--- constants/company.ts
+++ constants/company.ts
@@ -1,8 +1,8 @@
 export const mediaFolder = "/_vercel/image?url=%2F";
 
-export const companyName = "Mr. White - Undercover";
+export const companyName = "Undercover Mr. White";
 export const companyUrl = "https://mrwhite.fun";
-export const companySEOTitle = `${companyName} - Gioco di Società Online`;
+export const companySEOTitle = `${companyName} - Gioco di Parole e Bluff`;
 export const companySEODescription =
-  `Gioca online a ${companyName} con amici usando un solo telefono! Scopri chi ha la parola diversa in questo divertente party game di bluff italiano.`;
+  `Scopri chi ha la parola diversa in questo divertente party game di bluff italiano. Gioca ora a ${companyName} con i tuoi amici! `;
 export const companyLogo = `${companyUrl}${mediaFolder}logo.png&w=1536&q=100`;
