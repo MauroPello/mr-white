b90cf58d782f90dccbb5a88f4dfa81925116d399
MauroPello <mauro.pellonara@gmail.com>
2025-04-06 21:18:31 +0200

    yo

--- constants/company.ts
+++ constants/company.ts
@@ -4,7 +4,7 @@ export const companyName = "Undercover Mr. White";
 export const companyUrl = "https://mrwhite.fun";
 export const companySEOTitle = `${companyName} - Gioco Online di Parole`;
 export const companySEODescription =
-  `Scopri chi ha la parola diversa in questo divertente party game di bluff italiano. Gioca ora con i tuoi amici! `;
+  `Scopri chi ha la parola diversa in questo divertente party game di bluff italiano. Gioca ora con i tuoi amici!`;
 export const companyLogo = `${companyUrl}${mediaFolder}logo.png&w=1536&q=100`;
 export const companyMainStructuredData = {
   title: companySEOTitle,
