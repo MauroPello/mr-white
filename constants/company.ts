0665f84346ebbded6af1cf477c375ba0bd65b26f
Mauro Pellonara <mauro.pellonara@gmail.com>
2025-04-16 21:04:54 +0200

    remove dash

--- constants/company.ts
+++ constants/company.ts
@@ -2,7 +2,7 @@ export const mediaFolder = "/_vercel/image?url=%2F";
 
 export const companyName = "Undercover Mr. White";
 export const companyUrl = "https://mrwhite.fun";
-export const companySEOTitle = `${companyName} - Gioco Online di Parole`;
+export const companySEOTitle = `${companyName} Gioco Online di Parole`;
 export const companySEODescription =
   `Scopri chi ha la parola diversa in questo divertente party game di bluff italiano. Gioca ora con i tuoi amici!`;
 export const companyLogo = `${companyUrl}${mediaFolder}logo.png&w=1536&q=100`;
